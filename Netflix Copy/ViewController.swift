b6db13654e970af658a521c926cc12ab79a16d79
Mariusz Zając <zajac@gmx.us>
2022-10-23 10:24:11 +0200

    Initial Commit

---
+++ Netflix Copy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Copy
+//
+//  Created by Mariusz Zając on 23/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
