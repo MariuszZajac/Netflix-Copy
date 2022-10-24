ff26d8e6bd4bc539e671727d4d960a71e7b83209
Mariusz Zając <zajac@gmx.us>
2022-10-24 20:39:01 +0200

    add title to upcoming

--- Netflix Copy/Controllers/UpcomingViewController.swift
+++ Netflix Copy/Controllers/UpcomingViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class UpcomingViewController: UIViewController {
 
+    private var titles: [Title] = [Title]()
+    
     private let upcominaTable: UITableView = {
        let table = UITableView()
         table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -27,16 +29,37 @@ class UpcomingViewController: UIViewController {
         view.addSubview(upcominaTable)
         upcominaTable.delegate = self
         upcominaTable.dataSource = self
+        
+        fetchUpcoming()
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcominaTable.frame = view.bounds
     }
   
+    private func fetchUpcoming () {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcominaTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+                
+            }
+        }
+    }
 }
 extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = /*titles[indexPath.row].//orginal english title ??*/ titles[indexPath.row].original_title ?? "unknown"
         return cell
     }
 }
