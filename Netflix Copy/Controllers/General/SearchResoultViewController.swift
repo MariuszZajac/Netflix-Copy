b717b51a17cc58a19ae2fef1b759a15423d45587
Mariusz Zając <zajac@gmx.us>
2022-10-26 18:39:31 +0200

    searching update

--- Netflix Copy/Controllers/General/SearchResoultViewController.swift
+++ Netflix Copy/Controllers/General/SearchResoultViewController.swift
@@ -9,9 +9,9 @@ import UIKit
 
 class SearchResoultViewController: UIViewController {
    
-    private var titles: [Title] = [Title]()
+    public var titles: [Title] = [Title]()
 
-    private let searchResoultsCollectionView: UICollectionView = {
+    public let searchResoultsCollectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
         layout.itemSize = CGSize(width: UIScreen.main.bounds.width / 3 - 10, height: 200)
@@ -41,13 +41,14 @@ class SearchResoultViewController: UIViewController {
 }
 extension SearchResoultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TitleCollectionViewCell.identifier, for: indexPath) as? TitleCollectionViewCell else {
             return UICollectionViewCell()
         }
-        cell.backgroundColor = .blue
+        let title = titles[indexPath.row]
+        cell.configure(with: title.poster_path ?? "")
         return cell
     }
 }
